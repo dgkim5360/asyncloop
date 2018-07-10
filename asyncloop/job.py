ee144097cd2b7b40014df828f5397ab3a228f3c5
Don Kim <dgkimdev@gmail.com>
2018-07-10 20:29:47 +0900

    Replace the plain class with namedtuple

--- asyncloop/job.py
+++ asyncloop/job.py
@@ -1,6 +1,4 @@
-class PendingJob:
-    """A container to store a coroutine and its callback,
-    to be stored in the pending queue."""
-    def __init__(self, job_coro, callback=None):
-        self.job_coro = job_coro
-        self.callback = callback
+import collections
+
+
+PendingJob = collections.namedtuple('PendingJob', 'job_coro callback')
